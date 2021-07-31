8c89983dc8638039d3856186e7309b297b0c3279
triecie77 <williamslatriece@gmail.com>
2021-07-31 19:12:40 -0400

    changed port and added title

--- src/components/app.js
+++ src/components/app.js
@@ -4,8 +4,8 @@ export default class App extends Component {
   render() {
     return (
       <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <h2>React Bootstrap</h2>
+        <h1>Bottega Madlibs</h1>
+      
       </div>
     );
   }
